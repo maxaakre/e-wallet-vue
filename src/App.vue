60bedb3692567cf6239fa30a795e251d5eabfe3c
maxaakre <maxaakre@gmail.com>
2020-03-04 11:03:34 +0100

    Final commit

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,17 @@
 
 <style lang="scss">
 #app {
-      font-family: Avenir,Helvetica,Arial,sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
+    font-family: Avenir,Helvetica,Arial,sans-serif;
     font-family: PT Mono,monospace;
     padding: 1rem;
     height: 100vh;
     max-width: 24rem;
     margin: 0 auto;
-    -webkit-box-sizing: border-box;
     box-sizing: border-box;
 }
 html {
     display: block;
-    color: -internal-root-color;
+  
 }
 
 .div{
